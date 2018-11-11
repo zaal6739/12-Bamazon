2584323c4876ce10599d92ae746dfb9d2abeff85
Zack <zack1444@gmail.com>
2018-11-11 15:54:35 -0800

    finalized the script

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,13 +27,62 @@ var connection = mysql.createConnection({
 
     for (var j = 0; j < arrayLength; j++) {
     //displays what is for sale
-    console.log('---------------------------\n')
-    console.log('Product ID: ' + res[j].item_id);
-    console.log('Product Name: ' + res[j].product_name);
-    console.log('Product Price: ' + res[j].price+'\n');
+    console.log('---------------------------')
+    console.log('Product ID: ' + res[j].item_id+' Product Name: ' + res[j].product_name + ' Product Price: ' + res[j].price);
     }  
      
-    connection.end();
-
+//    connection.end();
+console.log('hit the arrow key for prompts');
     })
+    userInput()
+  }
+
+  function userInput() {
+    inquirer.prompt([
+      {
+        name: 'itemID',
+        type: 'input',
+        message:'What is the ID of the item you would like to purchase?'
+      },
+      {
+        name:'quantity',
+        type:'input',
+        message: 'How many items would you like to purchase?'
+      },
+    ]).then(function(answers) {
+     
+     var requestedID = answers.itemID;
+     var requestedQuantity = answers.quantity;
+     
+     productsPurchased(requestedID,requestedQuantity);
+    })
+
+  }
+
+  function productsPurchased(requestedID,requestedQuantity) {
+    connection.query(
+      "select * from bamazon.products where ?",
+      {
+        item_id: requestedID
+      },
+      function(err,res) {
+        if (err) throw err;
+       
+        if (requestedQuantity > res.stock_quantity)
+        {
+          console.log('Sorry, out of stock.  There are only '+res.stock_quantity+'items left!');
+        } 
+        else 
+        {
+          console.log ('Your item is still in stock!');
+
+          connection.query('UPDATE products set stock_quantity = stock_quantity - '+requestedQuantity+' where item_id ='+requestedID);
+         
+          var totalCost = res[0].price * requestedQuantity;
+
+          console.log('Your total cost is: '+ totalCost);
+
+        }
+     })
+ 
   }
